d17c13ce48ba338222e1f375ece569c6c3dd7ad6
Harris-Butt <33513710+Harris-Butt@users.noreply.github.com>
2020-12-10 15:30:36 +0500

    finish product page

--- src/Componenents/GenderSelector.js
+++ src/Componenents/GenderSelector.js
@@ -1,21 +1,31 @@
 import React, { useContext } from "react";
 import { Link } from "react-router-dom";
 import { AppContext } from "../App";
-
+import Button from "@material-ui/core/Button";
+import "./GenderSelector.css";
 function GenderSelector() {
   const appContext = useContext(AppContext);
   return (
     <div>
-      <Link to="men">
-        <button onClick={() => appContext.stateDispatch({ type: "men" })}>
-          Men
-        </button>
-      </Link>
-      <Link to="women">
-        <button onClick={() => appContext.stateDispatch({ type: "women" })}>
-          Women
-        </button>
-      </Link>
+      <div className="gender__selector">
+        <Link to="men">
+          <Button
+            onClick={() => appContext.stateDispatch({ type: "men" })}
+            color="primary"
+          >
+            Men
+          </Button>
+        </Link>
+        <Link to="women">
+          <Button
+            onClick={() => appContext.stateDispatch({ type: "women" })}
+            color="primary"
+          >
+            Women
+          </Button>
+        </Link>
+      </div>
+      <hr />
     </div>
   );
 }
